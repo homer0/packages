70c55a382b263c2bc5fd84040f074a35dff34f96
homer0 <leo.apiwan@olapic.com>
2019-08-07 05:01:48 -0300

    feat(eslint-plugin): add rules for the Jest environment

---
+++ packages/personal/eslint-plugin-homer0/rules/jest.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+    'node/no-missing-require': 'off',
+    'no-magic-numbers': 'off',
+  },
+};
