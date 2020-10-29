290093cd4d87837624f0c88a9546403175121ec3
homer0 <leo.apiwan@olapic.com>
2020-10-29 00:37:05 -0300

    docs(prettier-plugin-jsdoc): add missing documentation

--- packages/public/prettier-plugin-jsdoc/src/app.js
+++ packages/public/prettier-plugin-jsdoc/src/app.js
@@ -1,20 +1,31 @@
-/* eslint-disable jsdoc/require-jsdoc */
 const path = require('path');
-
+/**
+ * The actual dependency injection container. The keys will be the original functions, and the
+ * values may be the same, or the overrides.
+ *
+ * @type {Map<Function, Function>}
+ */
 const container = new Map();
+/**
+ * This secret exists so when {@link registerModule} gets called with a raw `module.exports`, it
+ * can safely skip a provider, as providers created with {@link provider} have this secret as a
+ * property.
+ *
+ * @type {symbol}
+ */
 const providerKey = Symbol('provider-secret');
 /**
- * Adds a new function to the app container.
+ * Adds a new function to the container.
  *
- * @param {Function} originalFn  The original function to add.
+ * @param {Function} originalFn  The reference for the original function.
  */
 const register = (originalFn) => {
   container.set(originalFn, originalFn);
 };
 /**
- * Overrides a function on the app container.
+ * Overrides a function on the container.
  *
- * @param {OG} originalFn  The reference to the original function that will be overriden.
+ * @param {OG} originalFn  The reference for the original function.
  * @param {OG} fn          The override function.
  * @template OG
  */
@@ -22,14 +33,26 @@ const override = (originalFn, fn) => {
   container.set(originalFn, fn);
 };
 /**
- * Gets a function or a function override from the app container.
+ * Gets a function or a function override from the container.
  *
- * @param {OG} originalFn  The reference to the original function.
+ * @param {OG} originalFn  The reference for the original function.
  * @returns {OG}
  * @template OG
  */
 const get = (originalFn) => container.get(originalFn) || originalFn;
-
+/**
+ * Registers a list or a dictionary of functions for a module. The idea of this function is that it
+ * can be easily use to register a `module.exports`. Check the example.
+ *
+ * @example
+ * module.exports.myFn = myFn;
+ * module.exports.myOtherFn = myOtherFn;
+ * registerModule('my-mod', module.exports);
+ *
+ * @param {string}                              id  The ID of the module; it will be added as the
+ *                                                  `moduleId` property of the functions.
+ * @param {Function[]|Object.<string,Function>} fns The list or dictionary of functions.
+ */
 const registerModule = (id, fns) => {
   const useFns = Array.isArray(fns) ? fns : Object.values(fns);
   useFns
@@ -40,23 +63,43 @@ const registerModule = (id, fns) => {
     register(fn);
   });
 };
-
+/**
+ * Creates a "module provider": a function that when called, it will execute {@link registerModule}.
+ * The idea of the providers is that they can be executed on runtime, or on a "registration step".
+ *
+ * @param {string}                              id  The ID of the module.
+ * @param {Function[]|Object.<string,Function>} fns The list or dictionary of functions.
+ * @returns {Function}
+ */
 const provider = (id, fns) => {
-  const fn = () => registerModule(id, fns);
+  /**
+   * The function that when executed will take care of registering the module.
+   */
+  const fn = () => {
+    registerModule(id, fns);
+  };
   fn.providerKey = providerKey;
   return fn;
 };
-
-const loadProviders = (directoryPath, list) => list
-.map((modName) => {
-  // eslint-disable-next-line global-require, import/no-dynamic-require
-  const { provider: modProvider } = require(path.join(directoryPath, `${modName}.js`));
-  return modProvider;
-})
-.filter((modProvider) => modProvider)
-.forEach((modProvider) => {
-  modProvider();
-});
+/**
+ * Loads a list of files, takes their `provider` export (if present) and executes it.
+ *
+ * @param {string}   directoryPath  The path of where the `list` of files is located.
+ * @param {string[]} list           A list of files with a `provider` export that should be
+ *                                  executed.
+ */
+const loadProviders = (directoryPath, list) => {
+  list
+  .map((modName) => {
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    const { provider: modProvider } = require(path.join(directoryPath, `${modName}.js`));
+    return modProvider;
+  })
+  .filter((modProvider) => modProvider)
+  .forEach((modProvider) => {
+    modProvider();
+  });
+};
 
 module.exports.register = register;
 module.exports.override = override;
--- packages/public/prettier-plugin-jsdoc/src/fns/getPlugin.js
+++ packages/public/prettier-plugin-jsdoc/src/fns/getPlugin.js
@@ -1,10 +1,32 @@
-/* eslint-disable jsdoc/require-jsdoc */
-
 const { getLanguages } = require('./getLanguages');
 const { getParsers } = require('./getParsers');
 const { getOptions, getDefaultOptions } = require('./getOptions');
 const { get, provider } = require('../app');
 
+/**
+ * @typedef {import('../types').PrettierSupportOption} PrettierSupportOption
+ * @typedef {import('../types').PJPOptions} PJPOptions
+ * @typedef {import('../types').PrettierParser} PrettierParser
+ * @typedef {import('./getLanguages').Language} Language
+ */
+
+/**
+ * @typedef {Object} Plugin
+ * @property {Language[]} languages
+ * The list of supported languages.
+ * @property {Object.<string,PrettierSupportOption>} options
+ * The options schema for the plugin.
+ * @property {PJPOptions} defaultOptions
+ * The plugin options with their default values.
+ * @property {Object.<string,PrettierParser>} parsers
+ * The dictionary of the parsers the plugin can use.
+ */
+
+/**
+ * Generates the plugin definition.
+ *
+ * @returns {Plugin}
+ */
 const getPlugin = () => ({
   languages: get(getLanguages)(),
   options: get(getOptions)(),
