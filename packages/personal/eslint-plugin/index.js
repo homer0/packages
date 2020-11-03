d84520063c23e20d7e18c5f39220de339691ac99
homer0 <leo.apiwan@olapic.com>
2020-11-02 23:52:58 -0300

    feat(eslint-plugin): add variants with Prettier

--- packages/personal/eslint-plugin/index.js
+++ packages/personal/eslint-plugin/index.js
@@ -3,13 +3,17 @@ const browser = require('./configurations/browser');
 const jest = require('./configurations/jest');
 const jsdoc = require('./configurations/jsdoc');
 const node = require('./configurations/node');
+const { addPrettier } = require('./utils');
 
 module.exports = {
   configs: {
     base,
     browser,
+    'browser-with-prettier': addPrettier(browser),
     jest,
+    'jest-with-prettier': addPrettier(jest),
     jsdoc,
     node,
+    'node-with-prettier': addPrettier(node),
   },
 };
