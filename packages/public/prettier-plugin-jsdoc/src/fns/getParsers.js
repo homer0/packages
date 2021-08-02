3c0626ca099b8e86ca1c6e20cb7fd83bbf46658b
homer0 <me@homer0.dev>
2021-08-02 01:13:46 -0300

    fix(prettier-plugin-jsdoc): use comment-parser cjs file

--- packages/public/prettier-plugin-jsdoc/src/fns/getParsers.js
+++ packages/public/prettier-plugin-jsdoc/src/fns/getParsers.js
@@ -1,5 +1,5 @@
 const R = require('ramda');
-const { parse: commentParser } = require('comment-parser/lib');
+const { parse: commentParser } = require('comment-parser/lib/index.cjs');
 const babelParser = require('prettier/parser-babel');
 const flowParser = require('prettier/parser-flow');
 const tsParser = require('prettier/parser-typescript');
