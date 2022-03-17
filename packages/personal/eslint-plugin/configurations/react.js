645cd45fe436f99721dd698c99681e962b5d28fb
homer0 <me@homer0.dev>
2022-03-17 04:52:25 -0300

    feat(eslint-plugin): add React config

---
+++ packages/personal/eslint-plugin/configurations/react.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: ['react', 'react-hooks'],
+  env: {
+    browser: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  extends: [
+    require.resolve('./browser.js'),
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    require.resolve('../rules/react.js'),
+  ],
+  globals: {
+    JSX: true,
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
--- packages/personal/eslint-plugin/index.js
+++ packages/personal/eslint-plugin/index.js
@@ -4,6 +4,7 @@ const jest = require('./configurations/jest');
 const jsdoc = require('./configurations/jsdoc');
 const node = require('./configurations/node');
 const nodeTypeScript = require('./configurations/node-typescript');
+const react = require('./configurations/react');
 const typescript = require('./configurations/typescript');
 const { addPrettier } = require('./utils');
 
@@ -12,6 +13,7 @@ const coreConfigs = Object.entries({
   jest,
   node,
   'node-typescript': nodeTypeScript,
+  react,
 }).reduce(
   (acc, [name, config]) => ({
     ...acc,
