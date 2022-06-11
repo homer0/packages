f8dcbdff87ff315f79914551d2e7a07a9a674af9
homer0 <me@homer0.dev>
2022-06-11 04:19:43 -0300

    feat(object-utils): add copy fn

---
+++ packages/public/object-utils/src/fns/copy.ts
@@ -0,0 +1,10 @@
+import { merge } from './merge';
+
+/**
+ * Creates a deep copy of a given object.
+ *
+ * @param target  The object to copy.
+ * @template T  The type of the input object, which will be the same as the one
+ *              returned.
+ */
+export const copy = <T>(target: T): T => merge(target);
