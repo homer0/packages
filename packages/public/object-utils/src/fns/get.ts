a3c4dbdaddd85faceff930547da01571ddfcf0ee
homer0 <me@homer0.dev>
2022-06-11 07:08:32 -0300

    feat(object-utils): add get fn

---
+++ packages/public/object-utils/src/fns/get.ts
@@ -0,0 +1,74 @@
+export type GetOptions = {
+  /**
+   * The object from where the property will be read.
+   */
+  target: unknown;
+  /**
+   * The path to the property.
+   */
+  path: string;
+  /**
+   * The delimiter that will separate the path components.
+   */
+  pathDelimiter?: string;
+  /**
+   * Whether or not to throw an error when the path is invalid. If this is `false`,
+   *  the method will silently fail and return `undefined`.
+   */
+  failWithError?: boolean;
+};
+
+/**
+ * Returns the value of an object property using a path.
+ *
+ * @param options  The options to use.
+ * @template T  The type of the returned object.
+ * @throws {Error} If the path is invalid and `failWithError` is set to `true`.
+ * @example
+ *
+ *   const obj = {
+ *     propOne: {
+ *       propOneSub: 'Charito!',
+ *     },
+ *     propTwo: '!!!',
+ *   };
+ *   console.log(get(obj, 'propOne.propOneSub'));
+ *   // Will output 'Charito!'
+ *
+ */
+export const get = <T>(options: GetOptions): T | undefined => {
+  const { target, path, pathDelimiter = '.', failWithError = false } = options;
+  const useTarget = target as Record<string, unknown>;
+  const parts = path.split(pathDelimiter);
+  const first = parts.shift()!;
+  let currentElement = useTarget[first] as Record<string, unknown>;
+  if (typeof currentElement === 'undefined') {
+    if (failWithError) {
+      throw new Error(`There's nothing on '${path}'`);
+    }
+
+    return undefined;
+  }
+
+  if (!parts.length) {
+    return currentElement as T;
+  }
+
+  let currentPath = first;
+  parts.some((currentPart) => {
+    let breakLoop = false;
+    currentPath += `${pathDelimiter}${currentPart}`;
+    currentElement = currentElement[currentPart] as Record<string, unknown>;
+    if (typeof currentElement === 'undefined') {
+      if (failWithError) {
+        throw new Error(`There's nothing on '${currentPath}'`);
+      } else {
+        breakLoop = true;
+      }
+    }
+
+    return breakLoop;
+  });
+
+  return currentElement as T;
+};
---
+++ packages/public/object-utils/tests/fns/get.test.ts
@@ -0,0 +1,120 @@
+jest.unmock('../../src/fns/get');
+
+import { get } from '../../src/fns/get';
+
+describe('get', () => {
+  it('should read a property from an object using its name', () => {
+    // Given
+    const target = {
+      city: 'Springfield',
+    };
+    // When
+    const result: string | undefined = get<string>({ target, path: 'city' });
+    // Then
+    expect(result).toBe(target.city);
+  });
+
+  it('should read a property from an object using a path', () => {
+    // Given
+    const name = 'Springfield';
+    const target = {
+      address: {
+        city: {
+          name,
+        },
+      },
+    };
+    // When
+    const result: string | undefined = get({ target, path: 'address.city.name' });
+    // Then
+    expect(result).toBe(name);
+  });
+
+  it('should read a property from an object using a custom path', () => {
+    // Given
+    const name = 'Springfield';
+    const target = {
+      address: {
+        city: {
+          name,
+        },
+      },
+    };
+    // When
+    const result: string | undefined = get({
+      target,
+      path: 'address/city/name',
+      pathDelimiter: '/',
+    });
+    // Then
+    expect(result).toBe(name);
+  });
+
+  it('should read a property from an object and support arrays', () => {
+    // Given
+    const name = 'Springfield';
+    const target = {
+      addressList: [
+        { city: null },
+        {
+          city: {
+            name,
+          },
+        },
+      ],
+    };
+    // When
+    const result: string | undefined = get({ target, path: 'addressList.1.city.name' });
+    // Then
+    expect(result).toBe(name);
+  });
+
+  it("shouldn't throw an error when trying to read a property that doesn't exist", () => {
+    // Given
+    const target = {};
+    const fakePath = 'something';
+    // When
+    const result = get({ target, path: fakePath });
+    // Then
+    expect(result).toBeUndefined();
+  });
+
+  it("shouldn't throw an error when trying to read a path that doesn't exist", () => {
+    // Given
+    const topElement = 'person';
+    const childElement = 'name';
+    const target = {
+      [topElement]: {},
+    };
+    const fakePath = `${topElement}.${childElement}`;
+    // When
+    const result = get({ target, path: fakePath, failWithError: false });
+    // Then
+    expect(result).toBeUndefined();
+  });
+
+  it("should throw an error when trying to read a property that doesn't exist", () => {
+    // Given
+    const target = {};
+    const fakePath = 'something';
+    // When/Then
+    expect(() => get({ target, path: fakePath, failWithError: true })).toThrow(
+      new RegExp(`there's nothing on '${fakePath}'`, 'i'),
+    );
+  });
+
+  it("should throw an error when trying to read a path that doesn't exist", () => {
+    // Given
+    const topElement = 'person';
+    const childElement = 'name';
+    const grandChildElement = 'first';
+    const target = {
+      [topElement]: {},
+    };
+    const fakePath = `${topElement}.${childElement}.${grandChildElement}`;
+    // When/Then
+    expect(() => get({ target, path: fakePath, failWithError: true })).toThrow(
+      new RegExp(`there's nothing on '${topElement}.${childElement}'`, 'i'),
+    );
+  });
+});
