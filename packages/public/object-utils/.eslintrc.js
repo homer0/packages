cece3fb41fb2aaefba9d562cd6d11119d0378f26
homer0 <me@homer0.dev>
2022-06-11 03:16:01 -0300

    chore(object-utils): configure ESLint and TS

---
+++ packages/public/object-utils/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@homer0'],
+  extends: ['plugin:@homer0/node-typescript-with-prettier'],
+  ignorePatterns: ['.eslintrc.js'],
+};
