a6258156913f704eaebb9a3d8db220950e5c0ef4
homer0 <me@homer0.dev>
2022-06-11 04:08:41 -0300

    fix(eslint-plugin): disable import rules for Jest

--- packages/personal/eslint-plugin/rules/jest.js
+++ packages/personal/eslint-plugin/rules/jest.js
@@ -2,6 +2,8 @@ module.exports = {
   rules: {
     'import/no-unresolved': 'off',
     'import/no-absolute-path': 'off',
+    'import/first': 'off',
+    'import/extensions': 'off',
     'node/no-missing-require': 'off',
     'no-magic-numbers': 'off',
   },
