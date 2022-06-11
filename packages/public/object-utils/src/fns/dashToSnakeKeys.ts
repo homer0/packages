475ce8b5e811c30fc7b57a29d408fc31c74039fa
homer0 <me@homer0.dev>
2022-06-11 05:12:16 -0300

    docs(object-utils): add missing jsdoc blocks

--- packages/public/object-utils/src/fns/dashToSnakeKeys.ts
+++ packages/public/object-utils/src/fns/dashToSnakeKeys.ts
@@ -1,5 +1,12 @@
 import { formatKeys, type FormatKeysExtensionOptions } from './formatKeys';
 
+/**
+ * A shorthand method for {@link formatKeys} that transforms the keys from `dash-case`
+ * to `snake_case`.
+ *
+ * @param options  The options to use.
+ * @template T  The type of the returned object.
+ */
 export const dashToSnakeKeys = <T>(options: FormatKeysExtensionOptions): T =>
   formatKeys<T>({
     ...options,
