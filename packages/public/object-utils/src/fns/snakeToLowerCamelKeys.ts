67c17b1775b808ac797011a2c96a62dce0112730
homer0 <me@homer0.dev>
2022-06-11 05:32:04 -0300

    feat(object-utils): add formatters for snake keys

---
+++ packages/public/object-utils/src/fns/snakeToLowerCamelKeys.ts
@@ -0,0 +1,18 @@
+import { formatKeys, type FormatKeysExtensionOptions } from './formatKeys';
+
+/**
+ * A shorthand method for {@link formatKeys} that transforms the keys from `snake_case` to
+ * `lowerCamelCase`.
+ *
+ * @param options  The options to use.
+ * @template T  The type of the returned object.
+ */
+export const snakeToLowerCamelKeys = <T>(options: FormatKeysExtensionOptions): T =>
+  formatKeys<T>({
+    ...options,
+    search: /([a-z])_([a-z])/g,
+    replace: (_, firstLetter, secondLetter) => {
+      const newSecondLetter = secondLetter.toUpperCase();
+      return `${firstLetter}${newSecondLetter}`;
+    },
+  });
