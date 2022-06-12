d3ed7bf9cb719c629b691daa706fb92e4abc6868
homer0 <me@homer0.dev>
2022-06-12 04:49:01 -0300

    feat(deep-assign): add export fns

---
+++ packages/public/deep-assign/src/index.ts
@@ -0,0 +1,20 @@
+import { DeepAssign } from './deepAssign';
+
+/**
+ * Makes a deep merge of a list of objects and/or arrays.
+ */
+export const deepAssign = DeepAssign.fn();
+/**
+ * Makes a deep merge of a list of objects and/or arrays, while concatenating arrays.
+ */
+export const deepAssignWithConcat = DeepAssign.fn({ arrayMode: 'concat' });
+/**
+ * Makes a deep merge of a list of objects and/or arrays, while overwriting existing
+ * arrays.
+ */
+export const deepAssignWithOverwrite = DeepAssign.fn({ arrayMode: 'overwrite' });
+/**
+ * Makes a deep merge of a list of objects and/or arrays, while replacing items in
+ * existing arrays.
+ */
+export const deepAssignWithShallowMerge = DeepAssign.fn({ arrayMode: 'shallowMerge' });
---
+++ packages/public/deep-assign/tests/index.test.ts
@@ -0,0 +1,130 @@
+jest.unmock('../src/deepAssign');
+jest.unmock('../src');
+
+import {
+  deepAssign,
+  deepAssignWithConcat,
+  deepAssignWithOverwrite,
+  deepAssignWithShallowMerge,
+} from '../src';
+
+describe('exports', () => {
+  describe('deepAssign', () => {
+    it('should have a function to call assign without an instance', () => {
+      // Given
+      const targetA = {
+        a: 'A',
+        b: 'B',
+      };
+      const targetB = {
+        b: 'X',
+        c: 'C',
+      };
+      const expected = {
+        ...targetA,
+        ...targetB,
+      };
+      // When
+      const result = deepAssign(targetA, targetB);
+      // Then
+      expect(result).toEqual(expected);
+    });
+
+    it('should have a function to merge arrays without concatenation', () => {
+      // Given
+      const targetA = {
+        a: 'A',
+        b: 'B',
+        d: ['D', 'E', 'F'],
+      };
+      const targetB = {
+        b: 'X',
+        c: 'C',
+        d: ['D', 'XE'],
+      };
+      const expected = {
+        ...targetA,
+        ...targetB,
+        d: ['D', 'XE', 'F'],
+      };
+      // When
+      const result = deepAssign(targetA, targetB);
+      // Then
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('deepAssignWithConcat', () => {
+    it('should have a function to merge arrays with concatenation', () => {
+      // Given
+      const targetA = {
+        a: 'A',
+        b: 'B',
+        d: ['D', 'E', 'F'],
+      };
+      const targetB = {
+        b: 'X',
+        c: 'C',
+        d: ['D', 'XE'],
+      };
+      const expected = {
+        ...targetA,
+        ...targetB,
+        d: [...targetA.d, ...targetB.d],
+      };
+      // When
+      const result = deepAssignWithConcat(targetA, targetB);
+      // Then
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('deepAssignWithOverwrite', () => {
+    it('should have a function to merge with arrays overwrite', () => {
+      // Given
+      const targetA = {
+        a: 'A',
+        b: 'B',
+        d: ['D', 'E', 'F'],
+      };
+      const targetB = {
+        b: 'X',
+        c: 'C',
+        d: ['D', 'XE'],
+      };
+      const expected = {
+        ...targetA,
+        ...targetB,
+      };
+      // When
+      const result = deepAssignWithOverwrite(targetA, targetB);
+      // Then
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('deepAssignWithShallowMerge', () => {
+    it('should have a function to do a shallow merge of two arrays', () => {
+      // Given
+      const targetA = {
+        a: 'A',
+        b: 'B',
+        d: [{ D: 'D' }, 'E', 'F'],
+      };
+      const targetB = {
+        b: 'X',
+        c: 'C',
+        d: [{ DX: 'DX' }, 'XE'],
+      };
+      const expected = {
+        ...targetA,
+        ...targetB,
+        d: [{ DX: 'DX' }, 'XE', 'F'],
+      };
+      // When
+      const result = deepAssignWithShallowMerge(targetA, targetB);
+      // Then
+      expect(result).toEqual(expected);
+    });
+  });
+});
