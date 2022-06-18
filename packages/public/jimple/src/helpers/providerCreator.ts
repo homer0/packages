6236ca7096cfa57631ed6916e4ee13b3438d67e0
homer0 <me@homer0.dev>
2022-06-18 07:57:25 -0300

    docs(jimple): add JSDoc comments

--- packages/public/jimple/src/helpers/providerCreator.ts
+++ packages/public/jimple/src/helpers/providerCreator.ts
@@ -1,7 +1,13 @@
 import type { Jimple } from '../jimple';
 import { resourceCreatorFactory, type GenericCurriedFn } from '../factories';
 import type { ProviderRegisterFn } from './provider';
-
+/**
+ * Generates a function to create "provider creators" for a specific type of container.
+ *
+ * @returns A function to generate provider creators.
+ * @template ContainerType  The type of the Jimple container (in case it gets
+ *                          subclassed).
+ */
 export const createProviderCreator = <ContainerType extends Jimple = Jimple>() => {
   type RegisterFn = ProviderRegisterFn<ContainerType>;
   type ProviderCreatorFn = GenericCurriedFn<RegisterFn>;
@@ -9,6 +15,19 @@ export const createProviderCreator = <ContainerType extends Jimple = Jimple>() =
   return <CreatorFn extends ProviderCreatorFn>(creator: CreatorFn) =>
     factory('provider', 'register', creator);
 };
-
+/**
+ * Generates a "provider creator": a provider that can also be called as a function in
+ * order to create a configured provider.
+ *
+ * @example
+ *
+ *   export const myService = providerCreator((options: Opts = {}) => (c) => {
+ *     c.set('myService', () => new MyService(options));
+ *   });
+ *
+ */
 export const providerCreator = createProviderCreator();
+/**
+ * A function that returns a provider registration function.
+ */
 export type ProviderCreatorFn = Parameters<typeof providerCreator>[0];
