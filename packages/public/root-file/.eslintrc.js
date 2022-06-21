6ede309dd25f03b69d6b8e7981b5024daadceaaf
homer0 <me@homer0.dev>
2022-06-21 09:16:20 -0300

    feat(root-file): add package

---
+++ packages/public/root-file/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@homer0'],
+  extends: ['plugin:@homer0/node-typescript-with-prettier'],
+  ignorePatterns: ['.eslintrc.js', 'dist/'],
+  rules: {
+    'node/no-extraneous-import': [
+      'error',
+      {
+        allowModules: ['@homer0/jimple', '@homer0/deferred', '@homer0/path-utils'],
+      },
+    ],
+    'node/no-missing-import': [
+      'error',
+      {
+        allowModules: ['package-json-type'],
+      },
+    ],
+  },
+};
