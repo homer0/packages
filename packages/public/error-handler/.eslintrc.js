6fd14e5c8e766793841a8aca08d03c61dea449c3
homer0 <me@homer0.dev>
2022-06-25 17:49:34 -0300

    feat(error-handler): add package

---
+++ packages/public/error-handler/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@homer0'],
+  extends: ['plugin:@homer0/node-typescript-with-prettier'],
+  ignorePatterns: ['.eslintrc.js', 'dist/'],
+  rules: {
+    'node/no-extraneous-import': [
+      'error',
+      {
+        allowModules: ['@homer0/jimple', '@homer0/simple-logger'],
+      },
+    ],
+  },
+};
