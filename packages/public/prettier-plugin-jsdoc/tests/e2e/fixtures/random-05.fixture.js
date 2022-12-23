051bea01d73c168827af0f03533823b03822a908
homer0 <me@homer0.dev>
2022-12-23 16:20:13 -0300

    fix: detect tags' names on a different line

---
+++ packages/public/prettier-plugin-jsdoc/tests/e2e/fixtures/random-05.fixture.js
@@ -0,0 +1,17 @@
+module.exports = {
+  printWidth: 90
+};
+
+//# input
+
+/**
+ * @typedef {import('./MemoriesAlgoliaRecord').MemoriesAlgoliaRecord}
+ * MemoriesAlgoliaRecord
+ */
+
+//# output
+
+/**
+ * @typedef {import('./MemoriesAlgoliaRecord').MemoriesAlgoliaRecord}
+ * MemoriesAlgoliaRecord
+ */
