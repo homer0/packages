920ab5127d745d4565323b5d9e3261b974a3d020
homer0 <me@homer0.dev>
2023-03-15 00:52:52 -0300

    fix(eslint-plugin): disable missing import on ts

--- packages/personal/eslint-plugin/rules/node-typescript.js
+++ packages/personal/eslint-plugin/rules/node-typescript.js
@@ -3,5 +3,6 @@ module.exports = {
     'import/no-unresolved': 'off',
     'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
     'node/no-unpublished-import': 'off',
+    'node/no-missing-import': 'off',
   },
 };
