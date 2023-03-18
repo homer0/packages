a99a583af4d35693ed2ee1219d7c7d07323e12ae
homer0 <me@homer0.dev>
2023-03-18 17:10:26 -0300

    fix(eslint-plugin): disable import/no-unresolved for ts

--- packages/personal/eslint-plugin/rules/node-typescript.js
+++ packages/personal/eslint-plugin/rules/node-typescript.js
@@ -1,6 +1,5 @@
 module.exports = {
   rules: {
-    'import/no-unresolved': 'off',
     'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
     'node/no-unpublished-import': 'off',
     'node/no-missing-import': 'off',
