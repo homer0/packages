7b679b05cbfeca1426cb121cf5fff1fdf8f0b7e8
homer0 <me@homer0.dev>
2023-07-16 08:52:32 -0300

    test(prettier-plugin-jsdoc): fix e2e tests

--- packages/public/prettier-plugin-jsdoc/tests/e2e/index.e2e.js
+++ packages/public/prettier-plugin-jsdoc/tests/e2e/index.e2e.js
@@ -11,15 +11,18 @@ describe(name, () => {
    * @type {Fixture[]}
    */
   const fixtures = global.e2eFixtures;
+  let plugin;
+  beforeAll(async () => {
+    plugin = await prettierPluginJSDoc();
+  });
+
   fixtures.forEach((fixture) => {
-    it(`should format fixture: ${fixture.name}`, () => {
-      const output = prettier
-        .format(fixture.input, {
-          plugins: [...(fixture.plugins || []), prettierPluginJSDoc],
-          ...fixture.options,
-        })
-        .trim();
-      expect(output).toBe(fixture.output);
+    it(`should format fixture: ${fixture.name}`, async () => {
+      const output = await prettier.format(fixture.input, {
+        plugins: [...(fixture.plugins || []), plugin],
+        ...fixture.options,
+      });
+      expect(output.trim()).toBe(fixture.output);
     });
   });
 });
