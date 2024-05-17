13bcedcad6ee117994af18ddde202ad83a572052
homer0 <me@homer0.dev>
2024-05-18 00:28:57 +0200

    chore(nestjs-zod-lite): add source code

---
+++ packages/public/nestjs-zod-lite/src/pipe.ts
@@ -0,0 +1,38 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ZodSchema } from 'zod';
+import { isZodDto, ZodDto } from './dto';
+import { ZodExceptionCreator } from './exception';
+import { validate } from './validate';
+
+interface ZodValidationPipeOptions {
+  createValidationException?: ZodExceptionCreator;
+}
+
+type ZodValidationPipeClass = new (schemaOrDto?: ZodSchema | ZodDto) => PipeTransform;
+
+export function createZodValidationPipe({
+  createValidationException,
+}: ZodValidationPipeOptions = {}): ZodValidationPipeClass {
+  @Injectable()
+  class ZodValidationPipe implements PipeTransform {
+    constructor(private schemaOrDto?: ZodSchema | ZodDto) {}
+
+    public transform(value: unknown, metadata: ArgumentMetadata) {
+      if (this.schemaOrDto) {
+        return validate(value, this.schemaOrDto, createValidationException);
+      }
+
+      const { metatype } = metadata;
+
+      if (!isZodDto(metatype)) {
+        return value;
+      }
+
+      return validate(value, metatype.schema, createValidationException);
+    }
+  }
+
+  return ZodValidationPipe;
+}
+
+export const ZodValidationPipe = createZodValidationPipe();
