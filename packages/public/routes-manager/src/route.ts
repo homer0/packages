accce889f7dcdc9dfe66a03469772906728e44d0
homer0 <me@homer0.dev>
2025-05-18 09:42:42 +0200

    docs(routes-manager): add docs to the route

--- packages/public/routes-manager/src/route.ts
+++ packages/public/routes-manager/src/route.ts
@@ -1,9 +1,30 @@
 import type { RoutePath, RoutePathParams } from './types';
-
+/**
+ * A utility class that wraps a route definition in order to provider helper methods for
+ * formatting.
+ */
 export class Route<Definition extends RoutePath = RoutePath> {
+  /**
+   * A list of all the components on a route path. This is created in the constructor and
+   * kept around in order to optimize the formatting process: in cases with parameters, we
+   * don't need to split the path again, look up the parameters and do the replacements;
+   * we just copy the list, get the indexes from `knownParamsIndex`, and replace the
+   * values.
+   */
   protected pathComponents: string[];
+  /**
+   * A map of all the parameters on the route path, and their indexes in `pathComponents`.
+   * This is used during formatting to avoid re-parsing the path and just replacing the
+   * component at the index.
+   */
   protected knownParamsIndex: Map<string, number> = new Map();
-
+  /**
+   * @param definition  The route definition to wrap.
+   * @throws An error if a param in the route path is not defined in the params
+   *         array.
+   * @throws An error if a param in the params array is not defined in the route
+   *         path.
+   */
   constructor(public readonly definition: Definition) {
     this.pathComponents = definition.path.split('/').filter(Boolean);
     const definitionParams = definition.params || [];
@@ -30,7 +51,16 @@ export class Route<Definition extends RoutePath = RoutePath> {
 
     this.format = this.format.bind(this);
   }
-
+  /**
+   * Creates a route path string from the definition.
+   *
+   * @param params  The parameters to replace in the route path. If the route has no
+   *                params, this can be omitted. Params not present in the definition
+   *                will be added as query parameters.
+   * @throws An error if the params are required but not provided.
+   * @throws An error if a required param is missing.
+   * @throws An error if a required query param is missing.
+   */
   format(...params: RoutePathParams<Definition>): string {
     const [paramsObj] = params;
     if (typeof paramsObj === 'undefined') {
