003a1bcd14f1187f8c9e2842edbacdba1cc76a50
homer0 <me@homer0.dev>
2025-11-10 04:15:54 +0100

    chore(nestjs-zod-lite): update tsup for esm only

--- packages/public/nestjs-zod-lite/tsup.config.ts
+++ packages/public/nestjs-zod-lite/tsup.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
   minify: !options.watch,
-  entry: ['src/**/*.ts'],
+  entry: ['src/**/*.ts', 'src/**/*.tsx'],
   sourcemap: true,
   clean: true,
-  format: ['esm', 'cjs'],
-  legacyOutput: true,
+  format: ['esm'],
   bundle: false,
+  splitting: false,
 }));
