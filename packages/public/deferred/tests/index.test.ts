a10d27371604bbfd5a82d173b396c4e2a7458664
homer0 <me@homer0.dev>
2025-11-10 03:04:12 +0100

    test(deferred): add .js extension to all imports

--- packages/public/deferred/tests/index.test.ts
+++ packages/public/deferred/tests/index.test.ts
@@ -1,6 +1,6 @@
-jest.unmock('../src');
+jest.unmock('../src/index.js');
 
-import { deferred } from '../src';
+import { deferred } from '../src/index.js';
 
 describe('deferred', () => {
   it('should be able to resolve a deferred promise', async () => {
