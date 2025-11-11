db4d15ad4b0cc696103e394da9538e25cd5cb267
homer0 <me@homer0.dev>
2025-11-11 05:41:43 +0100

    fix(ts-async-import): use esm syntax

--- packages/public/ts-async-import/src/index.js
+++ packages/public/ts-async-import/src/index.js
@@ -1,9 +1,9 @@
-module.exports.tsAsyncImport = (name, contextDir) => {
-  const importPath = contextDir
-    ? require.resolve(name, {
-        paths: [contextDir],
-      })
-    : name;
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+export function tsAsyncImport(name, contextDir) {
+  const importPath = contextDir ? require.resolve(name, { paths: [contextDir] }) : name;
   // eslint-disable-next-line node/no-unsupported-features/es-syntax
   return import(importPath);
-};
+}
