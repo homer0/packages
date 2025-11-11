bd6fef3c124847d2e5e0fd4af9de1b7a81fff2a3
homer0 <me@homer0.dev>
2025-11-11 02:17:33 +0100

    test(package-info): replace jest with vitest

--- packages/public/package-info/.eslintrc.cjs
+++ packages/public/package-info/.eslintrc.cjs
@@ -1,16 +1,6 @@
 const preset = require('@homer0/eslint-plugin/presets/typescript');
 
-module.exports = {
-  ...preset({
-    rootDir: __dirname,
-    configs: ['node-typescript-with-prettier'],
-  }),
-  rules: {
-    'node/no-missing-import': [
-      'error',
-      {
-        allowModules: ['package-json-type'],
-      },
-    ],
-  },
-};
+module.exports = preset({
+  rootDir: __dirname,
+  configs: ['node-typescript-with-prettier'],
+});
