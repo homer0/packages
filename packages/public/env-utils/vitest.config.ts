dd0edfbfe1763214d753a04308cacb8141e80d7f
homer0 <me@homer0.dev>
2025-11-11 01:32:08 +0100

    test(env-utils): replace jest with vitest

---
+++ packages/public/env-utils/vitest.config.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'node:path';
+import { defineConfig } from 'vitest/config';
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+  test: {
+    environment: 'node',
+    include: ['tests/**/*.{test,spec}.{ts,tsx,js,jsx}'],
+    exclude: ['node_modules/**'],
+    globals: true,
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: resolve('./coverage'),
+      include: ['src/**/*.{ts,tsx}'],
+      exclude: ['types.ts'],
+      reporter: ['text', 'lcov'],
+    },
+  },
+});
