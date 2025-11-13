27304182e549fdec6b1ebd1fe1d0bf8fab68c890
homer0 <me@homer0.dev>
2025-11-13 04:28:10 +0100

    chore(eslint-plugin): remove rules from root

--- packages/personal/eslint-plugin/rules/node-typescript.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  rules: {
-    'node/no-unsupported-features/es-syntax': ['error', { ignores: ['modules'] }],
-    'node/no-unpublished-import': 'off',
-    'node/no-missing-import': 'off',
-  },
-};
