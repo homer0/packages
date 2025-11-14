7098a957fb20555ef664ba28c38c054e2033cf62
homer0 <me@homer0.dev>
2025-11-14 23:04:19 +0100

    docs(eslint-plugin): update the links for the import plugin rules

--- packages/personal/eslint-plugin/src/airbnb/rules/imports.js
+++ packages/personal/eslint-plugin/src/airbnb/rules/imports.js
@@ -28,40 +28,40 @@ export default {
     // Static analysis:
 
     // ensure imports point to files/modules that can be resolved
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-unresolved.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-unresolved.md
     'import-x/no-unresolved': ['error', { commonjs: true, caseSensitive: true }],
 
     // ensure named imports coupled with named exports
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/named.md#when-not-to-use-it
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/named.md#when-not-to-use-it
     'import-x/named': 'error',
 
     // ensure default import coupled with default export
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/default.md#when-not-to-use-it
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/default.md#when-not-to-use-it
     'import-x/default': 'off',
 
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/namespace.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/namespace.md
     'import-x/namespace': 'off',
 
     // Helpful warnings:
 
     // disallow invalid exports, e.g. multiple defaults
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/export.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/export.md
     'import-x/export': 'error',
 
     // do not allow a default import name to match a named export
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-named-as-default.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-named-as-default.md
     'import-x/no-named-as-default': 'error',
 
     // warn on accessing default export property names that are also named exports
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-named-as-default-member.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-named-as-default-member.md
     'import-x/no-named-as-default-member': 'error',
 
     // disallow use of jsdoc-marked-deprecated imports
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-deprecated.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-deprecated.md
     'import-x/no-deprecated': 'off',
 
     // Forbid the use of extraneous packages
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-extraneous-dependencies.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-extraneous-dependencies.md
     // paths are treated both as absolute paths, and relative to process.cwd()
     'import-x/no-extraneous-dependencies': [
       'error',
@@ -95,41 +95,41 @@ export default {
     ],
 
     // Forbid mutable exports
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-mutable-exports.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-mutable-exports.md
     'import-x/no-mutable-exports': 'error',
 
     // Module systems:
 
     // disallow require()
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-commonjs.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-commonjs.md
     'import-x/no-commonjs': 'off',
 
     // disallow AMD require/define
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-amd.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-amd.md
     'import-x/no-amd': 'error',
 
     // No Node.js builtin modules
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-nodejs-modules.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-nodejs-modules.md
     // TODO: enable?
     'import-x/no-nodejs-modules': 'off',
 
     // Style guide:
 
     // disallow non-import statements appearing before import statements
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/first.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/first.md
     'import-x/first': 'error',
 
     // disallow duplicate imports
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-duplicates.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-duplicates.md
     'import-x/no-duplicates': 'error',
 
     // disallow namespace imports
     // TODO: enable?
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-namespace.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-namespace.md
     'import-x/no-namespace': 'off',
 
     // Ensure consistent use of file extension within the import path
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/extensions.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/extensions.md
     'import-x/extensions': [
       'error',
       'ignorePackages',
@@ -141,36 +141,36 @@ export default {
     ],
 
     // ensure absolute imports are above relative imports and that unassigned imports are ignored
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/order.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/order.md
     // TODO: enforce a stricter convention in module import order?
     'import-x/order': ['error', { groups: [['builtin', 'external', 'internal']] }],
 
     // Require a newline after the last import-x/require in a group
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/newline-after-import.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/newline-after-import.md
     'import-x/newline-after-import': 'error',
 
     // Require modules with a single export to use a default export
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/prefer-default-export.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/prefer-default-export.md
     'import-x/prefer-default-export': 'error',
 
     // Restrict which files can be imported in a given folder
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-restricted-paths.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-restricted-paths.md
     'import-x/no-restricted-paths': 'off',
 
     // Forbid modules to have too many dependencies
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/max-dependencies.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/max-dependencies.md
     'import-x/max-dependencies': ['off', { max: 10 }],
 
     // Forbid import of modules using absolute paths
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-absolute-path.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-absolute-path.md
     'import-x/no-absolute-path': 'error',
 
     // Forbid require() calls with expressions
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-dynamic-require.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-dynamic-require.md
     'import-x/no-dynamic-require': 'error',
 
     // prevent importing the submodules of other modules
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-internal-modules.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-internal-modules.md
     'import-x/no-internal-modules': [
       'off',
       {
@@ -180,22 +180,22 @@ export default {
 
     // Warn if a module could be mistakenly parsed as a script by a consumer
     // leveraging Unambiguous JavaScript Grammar
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/unambiguous.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/unambiguous.md
     // this should not be enabled until this proposal has at least been *presented* to TC39.
     // At the moment, it's not a thing.
     'import-x/unambiguous': 'off',
 
     // Forbid Webpack loader syntax in imports
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-webpack-loader-syntax.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-webpack-loader-syntax.md
     'import-x/no-webpack-loader-syntax': 'error',
 
     // Prevent unassigned imports
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-unassigned-import.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-unassigned-import.md
     // importing for side effects is perfectly acceptable, if you need side effects.
     'import-x/no-unassigned-import': 'off',
 
     // Prevent importing the default as if it were named
-    // https://github.com/un-ts/eslint-plugin-import-x/blob/main/docs/rules/no-named-default.md
+    // https://github.com/un-ts/eslint-plugin-import-x/blob/master/docs/rules/no-named-default.md
     'import-x/no-named-default': 'error',
 
     // Reports if a module's default export is unnamed
