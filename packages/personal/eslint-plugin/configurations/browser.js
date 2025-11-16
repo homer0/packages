c9d45b3a8dd3a6686e9084641f7ce20895586194
homer0 <me@homer0.dev>
2025-11-16 05:10:28 +0100

    chore(eslint-plugin): remove files from the root

--- packages/personal/eslint-plugin/configurations/browser.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    browser: true,
-  },
-  extends: ['./base.js', '../rules/transpilation.js'].map(require.resolve),
-};
--- packages/personal/eslint-plugin/configurations/jsdoc.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  plugins: ['jsdoc'],
-  extends: [require.resolve('../rules/jsdoc.js')],
-};
