5054a380bed1d222e894ec395a483ba3a231e2a1
homer0 <me@homer0.dev>
2025-11-17 03:52:12 +0100

    chore(eslint-plugin): prevent dist type checking

--- packages/personal/eslint-plugin/eslint.config.js
+++ packages/personal/eslint-plugin/eslint.config.js
@@ -3,6 +3,12 @@ import { createTsConfig } from './dist/presets/index.js';
 
 export default defineConfig([
   createTsConfig({
+    /**
+     * The reason for this is that we are importing the plugin from `dist`, which triggers type
+     * checking, which we don't want. The only alternative is to remove this file from the
+     * `tsconfig.json`, and that would trigger an ESLint error if we are not ignoring this file.
+     */
+    ignores: ['eslint.config.js'],
     importUrl: import.meta.url,
     configs: ['node-ts-with-prettier'],
   }),
