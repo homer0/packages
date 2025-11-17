dd7f9f7f002c46d7262f3fd67d16f2191b9de9e0
homer0 <me@homer0.dev>
2025-11-17 05:31:33 +0100

    fix(monorepo): restore bundling of the packages that had it

--- packages/public/simple-config/tsup.config.ts
+++ packages/public/simple-config/tsup.config.ts
@@ -2,10 +2,8 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig((options) => ({
   minify: !options.watch,
-  entry: ['src/**/*.ts', 'src/**/*.tsx'],
+  entry: ['src/index.ts'],
   sourcemap: true,
   clean: true,
   format: ['esm'],
-  bundle: false,
-  splitting: false,
 }));
