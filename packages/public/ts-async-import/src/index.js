8104c8e119cfeae2e84566374efb6574bf433a7b
homer0 <me@homer0.dev>
2025-11-17 03:23:24 +0100

    chore(ts-async-import): use new version of the eslint plugin

--- packages/public/ts-async-import/src/index.js
+++ packages/public/ts-async-import/src/index.js
@@ -4,6 +4,5 @@ const require = createRequire(import.meta.url);
 
 export function tsAsyncImport(name, contextDir) {
   const importPath = contextDir ? require.resolve(name, { paths: [contextDir] }) : name;
-  // eslint-disable-next-line node/no-unsupported-features/es-syntax
   return import(importPath);
 }
