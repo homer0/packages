62851c84136f0f2ba9e85a8d3d06cf49b743361f
homer0 <me@homer0.dev>
2025-11-17 03:13:14 +0100

    chore(path-utils): use new version of the eslint plugin

---
+++ packages/public/path-utils/eslint.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'eslint/config';
+import { createTsConfig } from '@homer0/eslint-plugin/presets';
+
+export default defineConfig([
+  createTsConfig({
+    importUrl: import.meta.url,
+    ignores: ['tests/**'],
+    configs: ['node-ts-with-prettier'],
+  }),
+  createTsConfig({
+    importUrl: import.meta.url,
+    files: 'all-inside:./tests',
+    configs: ['node-ts-tests-with-prettier'],
+    tsConfigPath: './tests',
+  }),
+]);
