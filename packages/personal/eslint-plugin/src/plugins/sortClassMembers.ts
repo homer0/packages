14b15a11a5b6cb6cd7765afe3da237eff3725e92
homer0 <me@homer0.dev>
2025-11-17 02:32:18 +0100

    fix(eslint-plugin): use a ts-expect-error on the import to avoid issues in the implementations

--- packages/personal/eslint-plugin/src/plugins/sortClassMembers.ts
+++ packages/personal/eslint-plugin/src/plugins/sortClassMembers.ts
@@ -1,9 +1,11 @@
+// @ts-expect-error - no types available
 import sortClassMembersPlugin from 'eslint-plugin-sort-class-members';
 import type { Linter } from 'eslint';
+import type { LinterPlugin } from '../commons/index.js';
 
 export const sortClassMembersPluginConfig: Linter.Config = {
   plugins: {
-    'sort-class-members': sortClassMembersPlugin,
+    'sort-class-members': sortClassMembersPlugin as LinterPlugin,
   },
   rules: {},
 };
--- packages/personal/eslint-plugin/src/plugins/sortClassMembersPlugin.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'eslint-plugin-sort-class-members' {
-  import type { LinterPlugin } from '../commons/index.js';
-
-  const sortClassMembersPluginConfig: LinterPlugin;
-
-  export default sortClassMembersPluginConfig;
-}
