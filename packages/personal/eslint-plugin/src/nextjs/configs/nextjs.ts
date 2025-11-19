2f6651e74361303ecddfd2254e8101e1cff5a425
homer0 <me@homer0.dev>
2025-11-19 05:07:12 +0100

    fix(eslint-plugin): add ts rules to the nextjs config

--- packages/personal/eslint-plugin/src/nextjs/configs/nextjs.ts
+++ packages/personal/eslint-plugin/src/nextjs/configs/nextjs.ts
@@ -3,12 +3,14 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import type { Config } from 'eslint/config';
 import { loadIgnorePathByEnvVar } from '../../utils/index.js';
-import { reactStyleRulesConfig } from '../../react/rules/index.js';
 import {
   baseRulesConfigs,
   basePluginsConfigs,
   baseLanguageOptionsConfig,
+  tsConfig,
 } from '../../configs/index.js';
+import { nodeTsRulesConfig } from '../../rules/index.js';
+import { reactStyleRulesConfig } from '../../react/rules/index.js';
 import { nextjsIgnoresRulesConfig } from '../rules/index.js';
 
 export const nextjsConfig: Config[] = [
@@ -17,6 +19,8 @@ export const nextjsConfig: Config[] = [
   ...baseRulesConfigs,
   ...nextVitals,
   ...nextTs,
+  ...tsConfig,
+  nodeTsRulesConfig,
   reactStyleRulesConfig,
   {
     ...baseLanguageOptionsConfig,
