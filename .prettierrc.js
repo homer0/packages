c699719088a26e140679fe26ff43cbea30b5d8be
homer0 <me@homer0.dev>
2026-08-09 11:28:43 +0200

    chore(monorepo): migrate root formatting to Oxfmt

--- .prettierrc.js
+++
@@ -1,8 +0,0 @@
-import base from '@homer0/prettier-config' with { type: 'json' };
-
-const config = {
-  ...base,
-  plugins: ['@homer0/prettier-plugin-jsdoc'],
-};
-
-export default config;
---
+++ oxfmt.config.ts
@@ -0,0 +1,5 @@
+import { createConfig } from '@homer0/oxfmt-config';
+
+export default createConfig({
+  ignores: ['**/tests/**/fixtures'],
+});
