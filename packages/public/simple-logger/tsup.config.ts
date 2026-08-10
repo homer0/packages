9ca72ce7ee9fbf52b77e4339bfdaa7ce8168aff4
homer0 <me@homer0.dev>
2026-08-10 04:29:14 +0200

    fix(simple-logger): build with tsdown

--- packages/public/simple-logger/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig((options) => ({
-  minify: !options.watch,
-  entry: ['src/index.ts'],
-  sourcemap: true,
-  clean: true,
-  format: ['esm'],
-}));
